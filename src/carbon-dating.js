6028fb3af4d359b4e2b58d5be634ae63e06f6a89
Whittun <whittun@gmail.com>
2023-12-13 02:03:23 +0300

    feat: add carbon task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  console.log(sampleActivity)
+  if (+sampleActivity > 0 && +sampleActivity < 15 && typeof sampleActivity === "string") {
+    const age = (Math.log(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD) / Math.log(2)
+    return Math.ceil(age)
+  } else {
+    return false
+  }
 }
 
 module.exports = {
