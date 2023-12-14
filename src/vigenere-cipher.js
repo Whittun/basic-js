f07e6049e28f009915fb167b383ad33561ac7d94
Whittun <whittun@gmail.com>
2023-12-14 21:12:59 +0300

    feat: add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type
+    this.letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!')
+
+    let text = message.toUpperCase()
+    let encryptedText = ''
+
+    let j = 0
+    for (let i = 0; i < text.length; i++) {
+      if (j === key.length) j = 0
+
+      if (this.letters.includes(text[i])) {
+        encryptedText += this.letters[(this.letters.indexOf(text[i]) + this.letters.indexOf(key.toUpperCase()[j])) % 26]
+        j++
+      } else {
+        encryptedText += text[i]
+      }
+    }
+
+    return (this.type) ? encryptedText : encryptedText.split('').reverse().join('')
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!')
+
+    let text = encryptedMessage.toUpperCase()
+    let decryptedText = ''
+
+    let j = 0
+    for (let i = 0; i < text.length; i++) {
+      if (j === key.length) j = 0
+
+      if (this.letters.includes(text[i])) {
+
+        let num = (this.letters.indexOf(text[i]) - this.letters.indexOf(key.toUpperCase()[j]))
+
+        decryptedText += this.letters[(num < 0) ? (num + 26) % 26 : num % 26]
+        j++
+      } else {
+        decryptedText += text[i]
+      }
+    }
+    return (this.type) ? decryptedText : decryptedText.split('').reverse().join('')
   }
 }
 
